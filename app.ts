02e14eec1e0eda080b847819c4fb0c9d08724b03
Drew Miller <drew.miller@limelighthealth.com>
2019-09-13 08:41:43 -0400

    first commit

---
+++ app.ts
@@ -0,0 +1,17 @@
+export class Hero {
+  id: number;
+  name: string;
+
+  constructor(name, id = 1) {
+      this.name = name;
+      this.id = id;
+  }
+
+  myName()
+  {
+      return this.name;
+  }
+}
+
+let hero = new Hero('Link');
+console.log(hero.myName());
---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+function getName(name) {
+    return name;
+}
+console.log(getName('Drew'));
